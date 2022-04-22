7912a967e7b7f17fa66e314c158df5bfdd691d53
Ross Johnson <RossJ24@users.noreply.github.com>
2022-04-22 19:29:00 -0400

    Add initial coordinator starter code

---
+++ coordinator/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	server "JKZDB/Coordinator/server"
+	"log"
+
+	fiber "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	coordinator, err := server.NewCoordinator("")
+	if err != nil {
+		log.Fatal("Could not start server.\n")
+	}
+	api := app.Group("/api", func(ctx *fiber.Ctx) error {
+		return ctx.Next()
+	})
+	api.Get("", coordinator.GetHandler)
+	api.Post("", coordinator.PostHandler)
+	api.Delete("", coordinator.DeleteHandler)
+
+}
---
+++ coordinator/server/server_lib.go
@@ -0,0 +1,28 @@
+package server_lib
+
+import (
+	fiber "github.com/gofiber/fiber/v2"
+)
+
+type Coordinator struct {
+	// Should map to grpc clients/connections
+	ShardMap      map[string]int
+	IndexedFields map[string]int
+}
+
+func NewCoordinator(shardConfigPath string) (*Coordinator, error) {
+	return nil, nil
+}
+
+func (coordinator *Coordinator) GetHandler(ctx *fiber.Ctx) error {
+	key := ctx.Query("key")
+
+}
+
+func (coordinator *Coordinator) PostHandler(ctx *fiber.Ctx) error {
+	key := ctx.Query("key")
+}
+
+func (coordinator *Coordinator) DeleteHandler(ctx *fiber.Ctx) error {
+	key := ctx.Query("key")
+}
